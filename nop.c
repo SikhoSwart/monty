25026f61a1deba0f3cf830bbc9c58295469d6611
SikhoSwart <sikhoswart@gmail.com>
2023-07-26 04:27:01 -0700

    Nop opcode

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * _nop - Implement the nop opcode
+ * @stack: pointer
+ * @line_number: current line number
+ * Return: nothing
+ */
+void _nop(__attribute__ ((unused))stack_t **stack,
+		__attribute__ ((unused)) unsigned int line_number)
+{
+	;
+}
